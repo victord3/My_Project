99b8e900530f849744016d83da0292ab8ba30a11
victord3 <victord3@msu.edu>
2022-10-18 14:51:32 -0400

    App 1-12

---
+++ script/app1-12.r
@@ -0,0 +1,72 @@
+{
+  rm(list=ls());  # cleans out the Environment every time the code is executed
+  options(show.error.locations = TRUE);  # show the line number of errors in the Console
+  
+  ### read in data from  twoWeekWeatherData.csv
+  weatherData = read.csv(file="twoWeekWeatherData3.csv", 
+                         sep=",",
+                         header=TRUE, 
+                         stringsAsFactors = FALSE);  
+  
+  #1
+  precipDesc = sort(weatherData$precipNA, decreasing = TRUE, na.last = TRUE)
+  precipAsc = sort(weatherData$precipNA, na.last = NA )
+  
+  #2
+  medianPrecipNA = median(weatherData$precipNA, na.rm = TRUE)
+  
+  #3
+  remainder = function(div1, div2)
+  {
+    remain = div1 %% div2
+    return(remain)
+  }
+  remainder(50, 9)
+  
+  #4
+  celciusToFahrenheit = function(celc)
+  {
+    fahr = (9/5)*(celc) + 32
+    return(fahr)
+  }
+  celciusToFahrenheit(0)
+  
+  #5
+  percentToLetterGrade = function(percent)
+  {
+    if(percent >= 90)
+    {
+      return("A")
+    }
+    else if(percent >= 80)
+    {
+      return("B")
+    }
+    else if(percent >= 70)
+    {
+      return("C")
+    }
+    else if(percent >= 60)
+    {
+      return("D")
+    }
+    else if(percent >= 50)
+    {
+      return("E")
+    }
+    else if(percent > 0)
+    {
+      return("F")
+    }
+    else if(percent < 0 | number > 100)
+    {
+      return("Error")
+    }
+  }
+  percentToLetterGrade(60)
+  
+  #1. Very comfortable
+  #2. Nothing
+  #3. Nothing
+  
+}
