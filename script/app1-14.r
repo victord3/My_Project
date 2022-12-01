dcaf7a39c75d8124ec602eb64b97991909734d58
victord3 <victord3@msu.edu>
2022-12-01 15:25:05 -0500

    App 1-14 AND Updated Toolbox

---
+++ script/app1-14.r
@@ -0,0 +1,28 @@
+{
+  rm(list=ls());  # cleans out the Environment every time the code is executed
+  options(show.error.locations = TRUE);  # show the line number of errors in the Console
+  
+  #1.
+  weatherData = read.csv(file="data/twoWeekWeatherData3.csv", 
+                         sep=",",
+                         header=TRUE, 
+                         stringsAsFactors = FALSE);  
+  source(file="script/toolbox.r");
+  
+  #2.
+  test1 = counter2(weatherData$highTemp, 50, ">=")
+  test2 = counter2(weatherData$highTemp, 50, "<=")
+  test3 = counter2(weatherData$highTemp, 50, "!=")
+  
+  #3.
+  testError = counter2(weatherData$highTemp, 32, "")
+
+  #4
+  test4 = valueComparer(weatherData$highTemp, 2, ">")
+  
+  #5
+  test5 = meanSquareError(weatherData$highTemp)
+  
+  
+}
+
---
+++ script/toolbox.R
@@ -0,0 +1,116 @@
+# see if the first number is divisible by the second
+isDivisible = function(div1, div2)
+{
+  remainder = div1 %% div2;
+  if(remainder == 0)
+  {
+    return(TRUE);
+  } 
+  else
+  {
+    return (FALSE);
+  }
+}
+
+counter = function(vector, compareVal)
+{
+  vecLength = length(vector);  # get the length of the vector
+  countVal = 0;                # initialize the count to 0
+  for(val in 1:vecLength)      # go through each value in vector
+  {
+    # check if the vector value is greater than the compareVal
+    if(vector[val] > compareVal)
+    {
+      countVal = countVal + 1; # add one to the count
+    }
+  }
+  
+  return(countVal);  # return the count value to the caller
+}
+
+counter2 = function(vector, compareVal, conditionalOp=">")
+{
+  vecLength = length(vector);  # get the length of the vector
+  countVal = 0;                # initialize the count to 0
+  for(val in 1:vecLength)      # go through each value in vector
+  {
+    if(conditionalOp == ">" & vector[val] > compareVal)
+    {
+      countVal = countVal + 1; # add one to the count
+    }
+    else if(conditionalOp == "<" & vector[val] < compareVal)
+    {
+      countVal = countVal + 1; # add one to the count
+    }
+    else if(conditionalOp == "==" & vector[val] == compareVal)
+    {
+      countVal = countVal + 1; # add one to the count
+    }
+    else if(conditionalOp == ">=" & vector[val] >= compareVal)
+    {
+      countVal = countVal +1
+    }
+    else if(conditionalOp == "<=" & vector[val] <= compareVal)
+    {
+     countVal = countVal + 1 
+    }
+    else if(conditionalOp == "!=" & vector[val] != compareVal)
+    {
+      countVal = countVal + 1
+    }
+    else
+    {
+      print("Error")
+    }
+  }
+  return(countVal);    # return the count value to the caller
+}
+
+
+valueComparer = function(vector, compareVal, conditionalOp="<")
+{
+  trueOrFalse = 0
+  vecLength = length(vector);
+  for(val in 1:vecLength)
+  {
+    if(conditionalOp == ">" & vector[val] > compareVal)
+    {
+      trueOrFalse = trueOrFalse + 1
+    }
+    else if(conditionalOp == "==" & vector[val] == compareVal)
+    {
+      trueOrFalse = trueOrFalse + 1
+    }
+    else if(conditionalOp == "<" & vector[val] < compareVal)
+    {
+      trueOrFalse = trueOrFalse + 1
+    }
+  }
+  print(trueOrFalse)
+  
+  if(trueOrFalse > 0)
+  {
+    print("TRUE")
+  }
+  else if(trueOrFalse == 0)
+  {
+    print("FALSE")
+  }
+}       
+
+meanSquareError = function(vector)
+{
+  vecLength = length(vector)
+  vecTotal = 0
+  for(i in 1:vecLength)
+  {
+    vecTotal = vecTotal + vector[i]
+  }
+  vecMean = vecTotal / vecLength
+  summedData = 0
+  for(i in 1:vecLength)
+  {
+  summedData = (summedData) + (vector[i] - vecMean)^2
+  }
+  mse = (1/vecLength) * (summedData)
+}
